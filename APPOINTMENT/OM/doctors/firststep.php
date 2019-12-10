ac035550912d31a309b67e759df8fc02056f766b
ProgrammerJohny <43511264+ProgrammerJohny@users.noreply.github.com>
2019-12-10 22:33:28 +0100

    Update firststep.php
    delete <style> which were the same as in main.php

--- APPOINTMENT/OM/doctors/firststep.php
+++ APPOINTMENT/OM/doctors/firststep.php
@@ -29,19 +29,6 @@ function ShowLogin($komunikat=""){
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
 <head>
-	<style>
-	div.card-body {
-		margin : 5px;
-	}
-
-div.card-body:hover{
-transition-duration: 1s;
-	background-color : lightgreen;
-
-	cursor : pointer;
-}
-
-	</style>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
